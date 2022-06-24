ee82f7fcee47cb709aa99b9c065f055ea95b1a53
Konstanty <Kostu96@gmail.com>
2022-06-24 21:20:31 +0200

    Add imgui config file

---
+++ samples/hello_imgui/main.cpp
@@ -0,0 +1,48 @@
+/*
+ * Copyright (C) 2022 Konstanty Misiak
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#define JNG_DECLARE_MAIN
+#include <jng/jng.hpp>
+
+class SampleLayer :
+    public jng::Layer
+{
+public:
+    SampleLayer()
+    {
+        
+    }
+
+    void onUpdate(float /*dt*/) override
+    {
+        static bool open = true;
+        ImGui::ShowDemoWindow(&open);
+    }
+private:
+};
+
+class App :
+    public jng::Engine
+{
+public:
+    static constexpr unsigned int WindowWidth = 1600;
+    static constexpr unsigned int WindowHeight = 900;
+
+    App() :
+        Engine({
+            "Hello ImGui!",
+            WindowWidth,
+            WindowHeight,
+            RendererType::Renderer2D
+        })
+    {
+        getLayerStack().pushLayer(new SampleLayer{});
+    }
+};
+
+jng::Engine* createApp() {
+    return new App{};
+}
