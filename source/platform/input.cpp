36ee0119220cf7b1d426bf53aedac372b1b88285
Konstanty <Kostu96@gmail.com>
2022-07-04 16:58:10 +0200

    Remove useless casts

--- source/platform/input.cpp
+++ source/platform/input.cpp
@@ -15,21 +15,21 @@ namespace jng {
 
     bool Input::isKeyPressed(Key::KeyCode key)
     {
-        GLFWwindow* window = reinterpret_cast<GLFWwindow*>(Engine::get().getWindow().getNativeWindowHandle());
+        GLFWwindow* window = Engine::get().getWindow().getNativeWindowHandle();
         int state = glfwGetKey(window, static_cast<int>(key));
         return state == GLFW_PRESS || state == GLFW_REPEAT;
     }
 
     bool Input::isMouseButtonPressed(Mouse::MouseCode button)
     {
-        GLFWwindow* window = reinterpret_cast<GLFWwindow*>(Engine::get().getWindow().getNativeWindowHandle());
+        GLFWwindow* window = Engine::get().getWindow().getNativeWindowHandle();
         int state = glfwGetMouseButton(window, button);
         return state == GLFW_PRESS;
     }
 
     glm::vec2 Input::getMousePosition()
     {
-        GLFWwindow* window = reinterpret_cast<GLFWwindow*>(Engine::get().getWindow().getNativeWindowHandle());
+        GLFWwindow* window = Engine::get().getWindow().getNativeWindowHandle();
         double x, y;
         glfwGetCursorPos(window, &x, &y);
         return { static_cast<float>(x), static_cast<float>(y) };
